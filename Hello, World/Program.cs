58a3b3aae042f46537d66b141c39ce1683df7449
Chiheart94 <martasims94@gmail.com>
2023-05-17 16:09:13 +0100

    commit all

---
+++ Hello, World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Helloworld
+{
+
+}
+    
+    Public class program
+{
+
+}
+{
+    static void Main(string[]args);
+    {
+        //Console.WritelLine("Hello, World.");
+        byte number = 2;
+    int count = 4;
+    float totalNumber = 20.5f;
+    char character = 'c';
+    bool haveMoney = true;
+    }
+}
