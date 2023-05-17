495d1a957951c21d6e7b924cb0f222be8034e2c0
Chiheart94 <martasims94@gmail.com>
2023-05-17 16:01:06 +0100

    commit all

---
+++ Switch statements/Switch statements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switchDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfTheWeek = 1;
+
+            switch(daysOfTheWeek)
+            {
+                case 1:
+                    Console.WriteLine("chichi");
+                    break;
+                    case 2:
+                    Console.WriteLine("monday");
+                    break;
+                    case 3:
+                    Console.WriteLine("tuesday");
+                    break;
+                    case 4:
+                    Console.WriteLine("wednesday");
+                    break;
+                    case 10:
+                    Console.WriteLine("thursday");
+                    break;
+                    case 6:
+                    Console.WriteLine("friday");
+                    break;
+                    case 7:
+                    Console.WriteLine("saturday");
+                    break;
+
+
+            }
+        }
+    }
+}
